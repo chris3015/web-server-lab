87332aae375900052117f8a32cd34e14a1296696
chris3015 <113226192+chris3015@users.noreply.github.com>
2022-09-21 14:56:21 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,24 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 # Import socket module
 from socket import *
+# In order to terminte the program
 import sys
-# Create a TCP server socket
-#(AF_INET is used for IPv4 protocols)
-#(SOCK_STREAM is used for TCP)
 def webServer(serverPort=13331):
-    #serverPort=6789
     serverSocket = socket(AF_INET, SOCK_STREAM)
 
     #Prepare a sever socket
     #Fill in start
-    serverSocket.bind(('', serverPort))
+    serverSocket.bind(("", serverPort))
     serverSocket.listen(1)
-    print ('the web server is up on port:', serverPort)
+    print ('the web server is up on port and running:', serverPort)
     #Fill in end
 
     while True:
@@ -26,47 +17,38 @@ def webServer(serverPort=13331):
 
         print ('Ready to serve...')
 
-        # Set up a new connection from the client
-        connectionSocket, addr =serverSocket.accept() #Fill in start   #Fill in end
-
+        connectionSocket, addr = serverSocket.accept() #Fill in start -are you accepting connections?
         try:
 
-            message =connectionSocket.recv(1024)  #Fill in start #Fill in end
+            message = connectionSocket.recv(1024)
 
             filename = message.split()[1]
 
             f = open(filename[1:])
 
-            outputdata =f.read() #Fill in start #Fill in end
+            outputdata = b"Content-Type: text/html; charset=UTF-8\r\n"\
             print (outputdata)
-            #Send one HTTP header line into socket
-            #Fill in start#
+            #Send an HTTP header line into socket for a valid request. What header should be sent for a response that is ok?
+            #Fill in start
             connectionSocket.send('\nHTTP/1.1 200 OK\n\n'.encode())
             #Fill in end
 
-            # Send the content of the requested file to the connection socket
+            # Send the content of the requested file to the client
             for i in range(0, len(outputdata)):
                 connectionSocket.send(outputdata[i].encode())
-            connectionSocket.send("\r\n".encode())
-            connectionSocket.close()
+            connectionSocket.send("\r\n\r\n".encode())
+            connectionSocket.close() #closing the connection socket
 
         except IOError:
-            # Send HTTP response message for file not found
+            # Send response message for invalid request due to the file not being found (404)
             #Fill in start
             connectionSocket.send("\nHTTP/1.1 404 Not Found\n\n".encode())
             #Fill in end
             # Close the client connection socket
-                    #Fill in start
+            #Fill in start
             connectionSocket.close()
-    #Fill in end
+            #Fill in end
     serverSocket.close()
-    sys.exit()
+    sys.exit() # Terminate the program after sending the corresponding data
 if __name__ == "__main__":
       webServer(13331)
-
-
-# In[ ]:
-
-
-
-
